19b078b5d48b9a725320e6d527bcd3c82be154f1
madusankapradeep928928-tech <madusankapradeep928928@gmail.com>
2026-07-09 10:10:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import { AppKeyGate } from '@/components/AppKeyGate';
 import { routes } from './routes';
 
 const App: React.FC = () => {
+  // routes array එකෙන් පරණ මුල් පිටුව ("/") තියෙන Route එක අයින් කරනවා
+  const filteredRoutes = routes.filter(route => route.path !== '/');
+
   return (
     <Router>
       <AuthProvider>
@@ -20,14 +23,19 @@ const App: React.FC = () => {
               <div className="flex flex-col min-h-screen">
                 <main className="flex-grow">
                   <Routes>
-                    {routes.map((route, index) => (
+                    {/* මුල් පිටුවට ("/") ආපු ගමන් කෙලින්ම Billing පිටුවට Redirect කරනවා */}
+                    <Route path="/" element={<Navigate to="/billing" replace />} />
+                    
+                    {/* ඉතිරි සියලුම POS පිටු (Billing, Products, Login ආදිය) සාමාන්‍ය පරිදි ලෝඩ් වෙනවා */}
+                    {filteredRoutes.map((route, index) => (
                       <Route
                         key={index}
                         path={route.path}
                         element={route.element}
                       />
                     ))}
-                    <Route path="/" element={<Navigate to="/billing" replace />} />
+                    
+                    {/* වැරදි ලින්ක් එකක් ගැහුවොත් Login පිටුවට යනවා */}
                     <Route path="*" element={<Navigate to="/login" replace />} />
                   </Routes>
                 </main>
@@ -40,5 +48,3 @@ const App: React.FC = () => {
     </Router>
   );
 };
-
-export default App;
